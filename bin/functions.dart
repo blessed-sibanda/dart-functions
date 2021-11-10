c5906674e8cd0feaffb389fa932280adf57c1ed5
Blessed Sibanda <blessedsibanda.me@gmail.com>
2021-11-10 14:12:16 +0200

    anonymous functions

--- bin/functions.dart
+++ bin/functions.dart
@@ -1,44 +1,16 @@
 void main(List<String> arguments) {
-  const input = 12;
-  final output = compliment(input);
-  print(output);
-  helloPersonAndPet('Fluffy', 'Chris');
-  print(fullName('Ray', 'Wenderlich'));
-  print(fullName('Albert', 'Einstein', 'Professor'));
-  print(withinTolerance(value: 5));
-  print(withinTolerance(value: 15));
-  print(withinTolerance(value: 9, min: 7));
-  print(withinTolerance(value: 9, max: 8));
-  print(withinTolerance(value: 9, min: 7, max: 11));
-  print(withinTolerance(value: 9, max: 11, min: 7));
-  print(youAreWonderful(name: 'Blessed', numberPeople: 5));
-  print(youAreWonderful(name: 'Chiedza'));
+  print(multiply(2, 3));
+  final triple = applyMultiplier(3);
+  print(triple(6));
+  print(triple(14.0));
 }
 
-String compliment(int number) {
-  return '$number is a very nice number';
-}
+final multiply = (int a, int b) {
+  return a * b;
+};
 
-void helloPersonAndPet(String person, String pet) {
-  print('Hello, $person, and your furry friend, $pet!');
-}
-
-String fullName(String first, String last, [String? title]) {
-  if (title != null) {
-    return '$title $first $last';
-  } else {
-    return '$first $last';
-  }
-}
-
-bool withinTolerance({
-  required int value,
-  int min = 0,
-  int max = 10,
-}) {
-  return min <= value && value <= max;
-}
-
-String youAreWonderful({required String name, int numberPeople = 30}) {
-  return "You're wonderful, $name. $numberPeople people think so.";
+Function applyMultiplier(num multiplier) {
+  return (num value) {
+    return value * multiplier;
+  };
 }
