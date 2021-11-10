736610579d42450b612f72cbc32af397361638b9
Blessed Sibanda <blessedsibanda.me@gmail.com>
2021-11-10 14:18:18 +0200

    closures

--- bin/functions.dart
+++ bin/functions.dart
@@ -1,16 +1,12 @@
 void main(List<String> arguments) {
-  print(multiply(2, 3));
-  final triple = applyMultiplier(3);
-  print(triple(6));
-  print(triple(14.0));
-}
-
-final multiply = (int a, int b) {
-  return a * b;
-};
-
-Function applyMultiplier(num multiplier) {
-  return (num value) {
-    return value * multiplier;
+  var counter = 0;
+  final incrementCounter = () {
+    counter += 1;
   };
+  incrementCounter();
+  incrementCounter();
+  incrementCounter();
+  incrementCounter();
+  incrementCounter();
+  print(counter);
 }
