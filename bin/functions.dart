ed7aa25d734bfdfba177b05e4185ee1d4e30d0dd
Blessed Sibanda <blessedsibanda.me@gmail.com>
2021-11-10 14:21:03 +0200

    more closures

--- bin/functions.dart
+++ bin/functions.dart
@@ -1,12 +1,20 @@
+// ignore_for_file: prefer_function_declarations_over_variables
+
 void main(List<String> arguments) {
+  var counter1 = countingFunction();
+  var counter2 = countingFunction();
+  print(counter1());
+  print(counter2());
+  print(counter1());
+  print(counter1());
+  print(counter2());
+}
+
+Function countingFunction() {
   var counter = 0;
   final incrementCounter = () {
     counter += 1;
+    return counter;
   };
-  incrementCounter();
-  incrementCounter();
-  incrementCounter();
-  incrementCounter();
-  incrementCounter();
-  print(counter);
+  return incrementCounter;
 }
